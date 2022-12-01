4e5d99520fa93ff080c7b6c1e34c04af0b3f074f
Ugo1000 <ugo.samuel.java@gmail.com>
2022-12-01 20:02:42 +0000

    calculator imprt

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,49 +1,44 @@
 #!/usr/bin/python3
-def add(a,b):
-	"""My Addition function
 
-	Args:
-		 a: first integer
-		 b: second integer
-		
-	Returns:
-	The return value . a + b 
-	"""
-	return (a + b)
+def add(a, b):
+    """My addition function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
 
 
 def sub(a, b):
-	""" My Subttraction function
+    """My subtraction function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
 
-	Args:
-		a: first integer
-		b: second integer
-
-	Returns:
-		The return values . a - b
-	"""
-	return (a - b)
 
 def mul(a, b):
-	 """My multiplication function
-	 Args:
-	 	a: first integer
-	  	b: second integer
-
-	Returns:
-		The return value. a * b
-	 """
-	 return (a * b)
+    """My multiplication function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
 
 
 def div(a, b):
-	"""My Division function
-
-	Args:
-		a: first integer
-		b: second integer
-
-	Returns:
-		The return value a / b
-	"""
-	return int (a / b)
+    """My division function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
