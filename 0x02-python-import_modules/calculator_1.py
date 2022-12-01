6bc493df28ac293520052d164356816e57431034
Ugo1000 <ugo.samuel.java@gmail.com>
2022-12-01 18:58:30 +0000

    imports caculator_1

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+def add(a,b):
+	"""My Addition function
+
+	Args:
+		 a: first integer
+		 b: second integer
+		
+	Returns:
+	The return value . a + b 
+	"""
+	return (a + b)
+
+
+def sub(a, b):
+	""" My Subttraction function
+
+	Args:
+		a: first integer
+		b: second integer
+
+	Returns:
+		The return values . a - b
+	"""
+	return (a - b)
+
+def mul(a, b):
+	 """My multiplication function
+	 Args:
+	 	a: first integer
+	  	b: second integer
+
+	Returns:
+		The return value. a * b
+	 """
+	 return (a * b)
+
+
+def div(a, b):
+	"""My Division function
+
+	Args:
+		a: first integer
+		b: second integer
+
+	Returns:
+		The return value a / b
+	"""
+	return int (a / b)
