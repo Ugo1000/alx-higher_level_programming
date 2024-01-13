4bb14aef054ba1ae184be1e6648ac8bedca6541f
Ugo1000 <ugo.samuel.java@gmail.com>
2024-01-13 01:27:17 +0100

    Insert value to first Table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,7 @@
 -- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+INSERT INTO
+    `first_table` (`id`, `name`)
+VALUES
+    (89, "Best School");
+
+-- insert this to the table first_table
