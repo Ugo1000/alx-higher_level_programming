9a40f380cfca64084462d687ea9efef8ab0dbef9
Ugo1000 <ugo.samuel.java@gmail.com>
2024-01-13 02:07:10 +0100

    No cheating

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,8 @@
--- updates teh score of Bob to 10 int the 
--- second table
+-- updates Bob score to 10
+-- second_table
 -- using only the name field
-UPDATE second_table SET score = 10 WHERE name = 'Bob';
+UPDATE second_table
+SET
+    score = 10
+WHERE
+    name = 'Bob';
