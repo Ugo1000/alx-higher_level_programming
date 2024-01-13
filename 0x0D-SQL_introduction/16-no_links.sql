c727faf096df31272af6bf8646b096fd575016ea
Ugo1000 <ugo.samuel.java@gmail.com>
2024-01-13 08:15:13 +0100

    Say my name

--- 0x0D-SQL_introduction/16-no_links.sql
+++ 0x0D-SQL_introduction/16-no_links.sql
@@ -1,2 +1,10 @@
 -- lista all records in a table except those wil no name value
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT
+    score,
+    name
+FROM
+    second_table
+WHERE
+    name IS NOT NULL
+ORDER BY
+    score DESC;
