f7f821a4eb523e1e5c6d4fd3abc07f7d21928449
Ugo1000 <ugo.samuel.java@gmail.com>
2024-01-13 01:31:08 +0100

    Count id with 89

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,8 @@
 -- count and display number of records where id=89
 -- in first_table
-SELECT COUNT(id) FROM first_table WHERE id = 89;i
+SELECT
+    COUNT(id)
+FROM
+    first_table
+WHERE
+    id = 89;
